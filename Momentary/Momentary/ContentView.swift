aeb270412cbb592f3ed7573bfb8faa4cc1b74222
Daniel Ngira <81624602+danielngira@users.noreply.github.com>
2025-01-21 16:38:06 -0600

    Added a groupbox and buttons to homepage

--- Momentary/Momentary/ContentView.swift
+++ Momentary/Momentary/ContentView.swift
@@ -10,12 +10,39 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Momentary")
+                .font(.title)
+            Spacer()
+            Text("Hello, User!")
+                .padding()
+            
+            GroupBox(label: Text("Daily Screen Time:"), content: {
+                Text("40 mins")
+            }
+            )
+            .backgroundStyle(Color.teal)
+            .foregroundColor(.white)
+            .frame(width: 200)
+            Spacer()
+            HStack {
+        
+                Button("Home"){
+                    //...
+                }
+                Spacer()
+                Button("Profile"){
+                    //
+                }
+                Spacer()
+                Button("Actions"){
+                    //
+                }
+            }
+            .foregroundColor(.white)
         }
         .padding()
+        .containerRelativeFrame([.horizontal, .vertical])
+        .background(Gradient(colors: [.orange, .yellow, .cyan, .blue, .teal, .green]).opacity(0.4))
     }
 }
 
