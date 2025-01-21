c7c152ef0fe42a60ba76243461bf9d5393162c6c
Daniel Ngira <81624602+danielngira@users.noreply.github.com>
2025-01-21 13:53:37 -0600

    Reconfigured App

--- Momentary/Momentary/MomentaryApp.swift
+++ Momentary/Momentary/MomentaryApp.swift
@@ -2,19 +2,16 @@
 //  MomentaryApp.swift
 //  Momentary
 //
-//  Created by Daniel Ngira on 1/20/25.
+//  Created by Daniel Ngira on 1/21/25.
 //
 
 import SwiftUI
 
 @main
 struct MomentaryApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
